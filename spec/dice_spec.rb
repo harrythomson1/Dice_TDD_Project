eed2272c1c79e62feebc4b41f6daac2c6fa99654
Harry Thomson <haroldt95@hotmail.com>
2021-01-27 12:21:41 +0000

    Completed second user story

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -4,7 +4,7 @@ describe Dice do
   
   describe "roll" do
     it "Rolls a dice" do
-      expect(subject.roll).to respond_to()
+      expect(subject.roll).to be_between(1, 6).inclusive
     end
   end
 end
